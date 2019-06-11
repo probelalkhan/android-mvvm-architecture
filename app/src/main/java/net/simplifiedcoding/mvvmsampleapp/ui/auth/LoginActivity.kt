1ee82f5cdb705df39071e8a68d9cc4bb4368415d
Belal Khan <probelalkhan@gmail.com>
2019-06-11 22:04:01 +0900

    Project Setup

---
+++ app/src/main/java/net/simplifiedcoding/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,15 @@
+package net.simplifiedcoding.mvvmsampleapp.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.simplifiedcoding.mvvmsampleapp.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
---
+++ app/src/main/java/net/simplifiedcoding/mvvmsampleapp/ui/auth/SignupActivity.kt
@@ -0,0 +1,13 @@
+package net.simplifiedcoding.mvvmsampleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.simplifiedcoding.mvvmsampleapp.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
