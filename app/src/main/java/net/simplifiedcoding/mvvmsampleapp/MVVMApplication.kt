8709a7dd33b04ce7a5b771df222940c7738b20ee
Belal Khan <probelalkhan@gmail.com>
2019-06-29 01:04:43 +0900

    Kodein Framework

--- app/src/main/java/net/simplifiedcoding/mvvmsampleapp/MVVMApplication.kt
+++ app/src/main/java/net/simplifiedcoding/mvvmsampleapp/MVVMApplication.kt
@@ -18,11 +18,13 @@ class MVVMApplication : Application(), KodeinAware {
 
     override val kodein = Kodein.lazy {
         import(androidXModule(this@MVVMApplication))
-        bind<NetworkConnectionInterceptor>() with singleton { NetworkConnectionInterceptor(instance()) }
+
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
         bind() from singleton { MyApi(instance()) }
         bind() from singleton { AppDatabase(instance()) }
         bind() from singleton { UserRepository(instance(), instance()) }
         bind() from provider { AuthViewModelFactory(instance()) }
+
     }
 
 }
