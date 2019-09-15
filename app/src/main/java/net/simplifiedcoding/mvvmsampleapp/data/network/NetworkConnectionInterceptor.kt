3d925416cdf42c67c1e7f0c766bc4642805da61b
Belal Khan <probelalkhan@gmail.com>
2019-09-15 19:31:53 +0900

    Updated to Target SDK 29

--- app/src/main/java/net/simplifiedcoding/mvvmsampleapp/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/net/simplifiedcoding/mvvmsampleapp/data/network/NetworkConnectionInterceptor.kt
@@ -2,6 +2,7 @@ package net.simplifiedcoding.mvvmsampleapp.data.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import net.simplifiedcoding.mvvmsampleapp.util.NoInternetException
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -13,17 +14,25 @@ class NetworkConnectionInterceptor(
     private val applicationContext = context.applicationContext
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        if(!isInternetAvailable())
+        if (!isInternetAvailable())
             throw NoInternetException("Make sure you have an active data connection")
         return chain.proceed(chain.request())
     }
 
-    private fun isInternetAvailable() : Boolean{
+    private fun isInternetAvailable(): Boolean {
+        var result = false
         val connectivityManager =
-            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.activeNetworkInfo.also {
-            return it != null && it.isConnected
+            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        connectivityManager?.let {
+            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
+                result = when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            }
         }
+        return result
     }
 
 }
--- app/src/main/java/net/simplifiedcoding/mvvmsampleapp/data/preferences/PreferenceProvider.kt
+++ app/src/main/java/net/simplifiedcoding/mvvmsampleapp/data/preferences/PreferenceProvider.kt
@@ -2,7 +2,7 @@ package net.simplifiedcoding.mvvmsampleapp.data.preferences
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 
 private const val KEY_SAVED_AT = "key_saved_at"
 
